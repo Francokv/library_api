e1cd4271a5b68b2371044cef981b6ed7f6585b7a
Franco Vallejos <fvallejosx@gmail.com>
2024-07-21 23:59:32 -0400

    crea autores de ejemplo

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+
+admin.site.register(Author, AuthorAdmin)
